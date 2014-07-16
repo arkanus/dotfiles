38aa1ec484acc3b3beabde95ee569c87f2dd21bc
Marcos Sánchez <arkanus@gmail.com>
2014-07-16 17:07:54 -0400

    Install tmux.conf and some install checks

--- install.sh
+++ install.sh
@@ -2,14 +2,21 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-files="gitconfig gvimrc vimrc vim bash bashrc gitignore_global"
+files="gitconfig gvimrc vimrc vim bash bashrc gitignore_global tmux.conf"
 
 mkdir -p ~/dotfiles.old
 
 for file in $files; do
-	mv ~/.$file ~/dotfiles.old/
-	ln -s $DIR/$file ~/.$file
+	if [ -d $file ]; then
+		cp ~/.$file ~/dotfiles.old/
+		ln -s $DIR/$file ~/.$file
+	fi
 done
 
 #Installs the bundles configured in .vimr using Vundle
 vim +BundleInstall +qa
+
+#Use only the snippets defined in snipmate-snippets
+if [ -d "$HOME/.vim/bundle/snipmate.vim/snippets" ]; then
+	mv $HOME/.vim/bundle/snipmate.vim/snippets $HOME/.vim/bundle/snipmate.vim/old.snippets
+fi
